85cf33503010321eab801150e3e92a7b3fec7d90
Masaki Ohno <masakio@post.kek.jp>
2019-07-10 11:59:54 +0900

    ドキュメントの構造を自動的に判別出来るように修正

--- scripts/file_add_script.py
+++ scripts/file_add_script.py
@@ -12,18 +12,12 @@ signal.signal(signal.SIGINT, lambda sig, frame: sys.exit('\n'))
 # コマンドライン引数処理
 parser = argparse.ArgumentParser(description='ファイルを実験データに追加するスクリプト')
 # parser.add_argument('-c', '--collection', help='collection name.')
-# parser.add_argument('-o', '--objectid', help='objectid str.')
 parser.add_argument('objectid', help='objectid str.')
 parser.add_argument('path', help='file or Dir path.')
 # クエリは structureがembの時だけ
 parser.add_argument('-q', '--query', default=None,
                     help='Ref is ObjectId or Emb is query list strings.')
-parser.add_argument('-s', '--structure', default='ref',
-                    help='Select ref(Reference, default) or emb(embedded).')
 args = parser.parse_args()
-
-# クエリの変換
-query = Action.file_query_eval(args.query, args.structure)
 
 # iniファイル読み込み
 settings = configparser.ConfigParser()
@@ -36,9 +30,14 @@ file = File(db.get_db)
 # 対象oidの所属コレクションを自動的に取得 ※動作が遅い場合は使用しないこと
 collection = db.find_collection_from_objectid(args.objectid)
 
+# ドキュメント構造の取得
+structure = db.get_structure(collection, args.objectid)
+
+# クエリの変換
+query = Action.file_query_eval(args.query, structure)
+
 if file.add_file_reference(collection, args.objectid,
-                           Action.files_read(args.path),
-                           args.structure, query):
+                           Action.files_read(args.path), structure, query):
     print('更新しました')
 else:
     print('更新に失敗しました')
