c79648ff4240bc484a346df42cf7e5948613ea13
Masaki Ohno <310414+ryde@users.noreply.github.com>
2020-03-30 11:47:19 +0900

    独自例外を追加

---
+++ edman/exceptions.py
@@ -0,0 +1,27 @@
+class EdmanError(Exception):
+    def __init__(self, message):
+        self._message = message
+
+    def __str__(self):
+        return f'{self.__class__.__name__} {self._message}'
+
+
+class EdmanDbConnectError(EdmanError):
+    """
+    DB接続に関するエラー
+    """
+    pass
+
+
+class EdmanDbProcessError(EdmanError):
+    """
+    DB実行処理に関するエラー
+    """
+    pass
+
+
+class EdmanInternalError(EdmanError):
+    """
+    Edmanの処理に関するエラー
+    """
+    pass
